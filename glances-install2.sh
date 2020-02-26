728917123676c975c056b184d4589158fd12161c
wmxc <55127640+wmxc@users.noreply.github.com>
2020-02-26 17:23:19 +0800

    Create glances-install2.sh

---
+++ glances-install2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "Installing curl..."
+yum install -y curl
+
+echo "Installing glances..."
+curl -L https://bit.ly/glances | /bin/bash
+
+echo "Installing pip bottle..."
+pip install bottle
+
+echo "Configuring glancesweb..."
+(
+cat <<EOF
+[Unit]
+Description = Glances in Web Server Mode
+After = network.target
+[Service]
+ExecStart = /usr/bin/glances  -w  -t  5
+[Install]
+WantedBy = multi-user.target
+EOF
+) > /etc/systemd/system/glancesweb.service
+
+systemctl enable glancesweb
+
+echo "Starting glancesweb..."
+systemctl start glancesweb
+
+echo "=================Glancesweb started================"
